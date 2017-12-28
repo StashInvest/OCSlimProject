70bf51f44302ac4611855ebf86f540bdca6a17de
mylegfeelsfunny <hatebyte@gmail.com>
2017-12-28 12:48:13 -0500

    • build test report file path so that is in the location the acceptanceUnitTestswrite it to. • improve error message with path

--- Pod/TestClasses/OCSPTestReportReader.m
+++ Pod/TestClasses/OCSPTestReportReader.m
@@ -6,18 +6,18 @@
     
     NSString *fitnesseTestReportResourceName = @"Fitnesse-Test-Report";
     
-    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    NSBundle *bundle            = [NSBundle bundleForClass:[self class]];
+    NSString *targetBundlePath  = [[bundle.bundlePath stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];
+    NSString *path              = [[targetBundlePath stringByAppendingPathComponent:fitnesseTestReportResourceName] stringByAppendingPathExtension:@"xml"];
     
-    NSString *path = [bundle pathForResource:fitnesseTestReportResourceName ofType:@"xml"];
-    
-    if (path == nil ) {
-       
-        NSLog(@"[OCSP] ERROR: Could not read data : The file \"%@.xml\" couldn’t be opened because there is no such file: Check your 'OCSlimProject' Acceptance Test target is being built. 1) Try running 'pod update' to resolve this issue. 2) Manually run Acceptance Tests using '$ ./LaunchFitnesse' located at your projects root directory and investigate any issues.", fitnesseTestReportResourceName, nil);
+    if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        
+        NSLog(@"[OCSP] ERROR: Could not read data : The file \"%@.xml\" couldn’t be opened because there is no such file at \"%@\": Check your 'OCSlimProject' Acceptance Test target is being built. 1) Try running 'pod update' to resolve this issue. 2) Manually run Acceptance Tests using '$ ./LaunchFitnesse' located at your projects root directory and investigate any issues.", targetBundlePath, path, nil);
         
     }
     
     return [NSData dataWithContentsOfFile:path];
-    
+
 }
 
 @end
